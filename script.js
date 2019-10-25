60f8ac46ef627fc08b5f7ef2257578fef65a68ed
ivan <ivanwolyniec@gmail.com>
2019-10-25 10:05:23 -0400

    changed up some messaging

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ function gameOn(choice = 1) {
   let feedback = ``;
   let computerChoice = Math.floor(Math.random() * 3 + 1);
   if (computerChoice === choice) {
-    feedback = `it's a tie! you both chose ${choice}`;
+    feedback = `It's a ${choice} Tie!`;
   } else if (computerChoice == choice + 1) {
-    feedback = `${choice} vs ${computerChoice} computer wins`;
+    feedback = `${choice} vs ${computerChoice} </br></br>DEFEAT`;
   } else if (choice == computerChoice + 1) {
-    feedback = `${choice} vs ${computerChoice} player wins`;
+    feedback = `${choice} vs ${computerChoice} </br></br>VICTORY`;
   } else if (choice == 1 && computerChoice == 3) {
-    feedback = `${choice} vs ${computerChoice} player wins`;
+    feedback = `${choice} vs ${computerChoice} </br></br>VICTORY`;
   } else if (choice == 3 && computerChoice == 1) {
-    feedback = `${choice} vs ${computerChoice} computer wins`;
+    feedback = `${choice} vs ${computerChoice} </br></br>DEFEAT!`;
   } else {
     feedback = `${choice} vs ${computerChoice}`;
   }
-  feedback = feedback.replace(1, "rock");
-  feedback = feedback.replace(2, "paper");
-  feedback = feedback.replace(3, "scissors");
+  feedback = feedback.replace(1, "ROCK");
+  feedback = feedback.replace(2, "PAPER");
+  feedback = feedback.replace(3, "SCISSORS");
+
   document.getElementById("result").innerHTML = feedback;
 }
