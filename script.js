9be123848132efbeccb4f479a358715ff40cbdca
ivan <ivanwolyniec@gmail.com>
2019-10-26 14:52:09 -0400

    removed sloppy breaks

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 function gameOn(choice = 1) {
-  let feedback = ``;
+  let feedback = ["", ""];
   let computerChoice = Math.floor(Math.random() * 3 + 1);
   if (computerChoice === choice) {
-    feedback = `It's a ${choice} Tie!`;
+    feedback[0] = `It's a ${choice} Tie!`;
   } else if (computerChoice == choice + 1) {
-    feedback = `${choice} vs ${computerChoice} </br></br>DEFEAT`;
+    feedback[0] = `${choice} vs ${computerChoice}`;
+    feedback[1] = `DEFEAT`;
   } else if (choice == computerChoice + 1) {
-    feedback = `${choice} vs ${computerChoice} </br></br>VICTORY`;
+    feedback[0] = `${choice} vs ${computerChoice}`;
+    feedback[1] = `VICTORY`;
   } else if (choice == 1 && computerChoice == 3) {
-    feedback = `${choice} vs ${computerChoice} </br></br>VICTORY`;
+    feedback[0] = `${choice} vs ${computerChoice}`;
+    feedback[1] = `VICTORY`;
   } else if (choice == 3 && computerChoice == 1) {
-    feedback = `${choice} vs ${computerChoice} </br></br>DEFEAT!`;
+    feedback[0] = `${choice} vs ${computerChoice}`;
+    feedback[1] = `DEFEAT`;
   } else {
-    feedback = `${choice} vs ${computerChoice}`;
+    feedback[0] = `${choice} vs ${computerChoice}`;
   }
-  feedback = feedback.replace(1, "ROCK");
-  feedback = feedback.replace(2, "PAPER");
-  feedback = feedback.replace(3, "SCISSORS");
+  feedback[0] = feedback[0].replace(1, "ROCK");
+  feedback[0] = feedback[0].replace(2, "PAPER");
+  feedback[0] = feedback[0].replace(3, "SCISSORS");
 
-  document.getElementById("result").innerHTML = feedback;
+  document.getElementById("res").innerHTML = feedback[0];
+  document.getElementById("res2").innerHTML = feedback[1];
+  document.getElementById("res").focus();
 }
